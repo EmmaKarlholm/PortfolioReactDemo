f72814e4dd7b2ebce24676ad5e41089f16bcae07
EmmaKarlholm <emma.karlholm@gmail.com>
2026-03-20 16:07:18 +0100

    Simplified deploying to GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,26 @@
 import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client'
 import { StrictMode } from 'react'
 import App from './App.jsx'
 import "./style.css"
 
+const deployingToGitHubPages = false;
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+if (deployingToGitHubPages) {
+  createRoot(document.getElementById('root')).render(
+    <StrictMode>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </StrictMode>,
+  )
+} else {
+  createRoot(document.getElementById('root')).render(
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>,
+  )
+}
